8128a884315d338fafad4bbb53df97aa4b8e6d06
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-05 18:04:12 +0400

    feat: users implemented

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,32 @@
+const users = [];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return users;
 };
 
-module.exports = { getAll };
+const getOneById = async id => {
+  return users.find(el => el.id === id);
+};
+
+const postOne = async user => {
+  users.push(user);
+  return user;
+};
+
+const putOneById = async (id, user) => {
+  const indexById = users.findIndex(el => el.id === id);
+
+  if (indexById !== -1) {
+    const modified = { ...users[indexById], ...user };
+    users[indexById] = modified;
+    return modified;
+  }
+};
+
+const deleteOneById = async id => {
+  const indexById = users.findIndex(el => el.id === id);
+  if (indexById === -1) throw new Error('NOT_FOUND');
+  users.splice(indexById, 1);
+};
+
+module.exports = { getAll, getOneById, postOne, putOneById, deleteOneById };
