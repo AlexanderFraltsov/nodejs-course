9071fc73465a12e78fcd10ba3d7462cca4af413d
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-11 17:28:15 +0400

    refactor: cleaned tasks code

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -25,8 +25,11 @@ const putOneById = async (id, task) => {
 
 const deleteOneById = async id => {
   const indexById = tasks.findIndex(el => el.id === id);
-  if (indexById === -1) throw new Error('NOT_FOUND');
-  tasks.splice(indexById, 1);
+  const isDeleted = indexById !== -1;
+  if (isDeleted) {
+    tasks.splice(indexById, 1);
+  }
+  return isDeleted;
 };
 
 module.exports = { getAll, getOneById, postOne, putOneById, deleteOneById };
