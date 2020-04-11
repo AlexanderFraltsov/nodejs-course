3d177752479f0c0f306c979b9003cf3598e5a762
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-11 19:12:48 +0400

    feat: added function for parsing request to string

---
+++ src/utils/get-logs-from-request.js
@@ -0,0 +1,20 @@
+const getStringFromObject = require('./get-string-from-object');
+
+module.exports = req => {
+  const { url, method, body, query } = req;
+
+  const request = getStringFromObject(body);
+  const params = getStringFromObject(query);
+
+  const logToConsole = `incoming request:
+  {
+    url: ${url},
+    method: ${method},
+    body: ${request},
+    query_params: ${params}
+  }`;
+
+  const logToFile = `{ url: ${url}, method: ${method}, body: ${request}, query_params: ${params} }`;
+
+  return { logToConsole, logToFile };
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 const getStringFromObject = require('./get-string-from-object');
+const getLogsFromRequest = require('./get-logs-from-request');
 
-module.exports = { getStringFromObject };
+module.exports = { getStringFromObject, getLogsFromRequest };
