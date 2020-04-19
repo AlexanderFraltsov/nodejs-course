a2c231f5e85439e3410c44e53849688fded0d3f6
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-19 22:03:11 +0400

    feat: returned board validation at task-service

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,11 +1,17 @@
-const { NOT_FOUND } = require('http-status-codes');
+const { NOT_FOUND, BAD_REQUEST, getStatusText } = require('http-status-codes');
+const { TASK_NOT_FOUND } = require('../../common/constants').ERRORS;
 
 const { ErrorHandler } = require('../../common/error');
 const tasksRepo = require('./task.db.repository');
-const { TASK_NOT_FOUND } = require('../../common/constants').ERRORS;
+const boardsRepo = require('../boards/board.db.repository');
 const Task = require('./task.model');
 
 const getAll = async id => {
+  const board = await boardsRepo.getOneById(id);
+  if (!board) {
+    throw new ErrorHandler(BAD_REQUEST, getStatusText(BAD_REQUEST));
+  }
+
   const tasks = await tasksRepo.getAll();
 
   if (!tasks) return [];
@@ -18,6 +24,11 @@ const postOne = async (id, taskFields) => {
 };
 
 const getOneById = async (id, taskId) => {
+  const board = await boardsRepo.getOneById(id);
+  if (!board) {
+    throw new ErrorHandler(NOT_FOUND, TASK_NOT_FOUND);
+  }
+
   const task = await tasksRepo.getOneById(taskId);
   if (!task) {
     throw new ErrorHandler(NOT_FOUND, TASK_NOT_FOUND);
@@ -27,6 +38,11 @@ const getOneById = async (id, taskId) => {
 };
 
 const putOneById = async (id, taskId, task) => {
+  const board = await boardsRepo.getOneById(id);
+  if (!board) {
+    throw new ErrorHandler(BAD_REQUEST, getStatusText(BAD_REQUEST));
+  }
+
   const newTask = await tasksRepo.putOneById(taskId, task);
 
   if (!newTask) {
@@ -37,6 +53,11 @@ const putOneById = async (id, taskId, task) => {
 };
 
 const deleteOneById = async (id, taskId) => {
+  const board = await boardsRepo.getOneById(id);
+  if (!board) {
+    throw new ErrorHandler(BAD_REQUEST, getStatusText(BAD_REQUEST));
+  }
+
   const task = await tasksRepo.getOneById(taskId);
 
   if (!task) {
